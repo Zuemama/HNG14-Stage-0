e9d3f8ec2930cfd20f7735c774673bdf3c2521e7
Adeniyi <jasonzenith2017@gmail.com>
2026-04-12 11:07:59 +0100

    Add root route

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const express = require('express');
 
 const app = express();
 const classifyRoutes = require('./routes/classifyRoute');
-
-app.use('/api', classifyRoutes);
-
-//Root route
+// Root route
 app.get('/', (req, res) => {
   res.send('API is running 🚀');
 });
 
-// Example endpoint
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
+
+app.use('/api', classifyRoutes);
 
 
 
@@ -23,3 +18,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
